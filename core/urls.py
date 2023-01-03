3b27f0bb6fbdb3e91f07348a57d678613a81d30c
MumaBrian <brianmuma5@gmail.com>
2023-01-03 17:02:23 -0500

    project app

---
+++ core/urls.py
@@ -0,0 +1,8 @@
+from . import views
+from django.urls import path
+from django.contrib.auth import views as auth_views
+urlpatterns = [
+    path('',views.frontpage,name='frontpage'),
+    path('signup/',views.signup,name='signup'),
+    path('logout/',auth_views.LogoutView.as_view(),name="logout"),
+]
